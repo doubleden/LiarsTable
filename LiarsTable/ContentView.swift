c3f77a12405c4a1426ce381f47ffc3cd3a5f2ee5
Denis Denisov <denis.denisov8701@gmail.com>
2025-06-25 14:04:32 +0200

    Initial Commit

---
+++ LiarsTable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiarsTable
+//
+//  Created by Denis Denisov on 25/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiarsTable/LiarsTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiarsTableApp.swift
+//  LiarsTable
+//
+//  Created by Denis Denisov on 25/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LiarsTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
